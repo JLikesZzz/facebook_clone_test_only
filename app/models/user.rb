357be350738ffeb49048e5f19be0f2728adc58a4
JLikesZzz <jeremy_lzz@hotmail.com>
2016-06-06 11:19:33 +0800

    user model done left assosiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :likes, dependent: :destroy
+  has_many :statuses, dependent: :destroy
+
+  validates :name, :email, :password, :password_confirmation, presence: true, uniqueness: true
+  validates :password, :length => { :minimum => 6, :maximum => 20 }, :confirmation =>true
+  validates :email, format: { with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/}
+  validates_confirmation_of :password
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606025158) do
+ActiveRecord::Schema.define(version: 20160606030720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160606025158) do
     t.string   "password_confirmation"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.string   "password_digest"
   end
 
 end
